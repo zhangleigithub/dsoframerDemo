9e458e9ea4c17b5290224f6b7366fd8bfc9abac8
zhangleigithub <1535835796@qq.com>
2018-07-13 14:17:17 +0800

    添加项目文件。

---
+++ DsoframerDemo/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace DsoframerDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axFramerControl1 = new AxDSOFramer.AxFramerControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axFramerControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axFramerControl1
+            // 
+            this.axFramerControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axFramerControl1.Enabled = true;
+            this.axFramerControl1.Location = new System.Drawing.Point(0, 94);
+            this.axFramerControl1.Name = "axFramerControl1";
+            this.axFramerControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axFramerControl1.OcxState")));
+            this.axFramerControl1.Size = new System.Drawing.Size(618, 293);
+            this.axFramerControl1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "插入书签";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(143, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "设置书签值";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 387);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axFramerControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axFramerControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxDSOFramer.AxFramerControl axFramerControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DsoframerDemo/Form1.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DsoframerDemo
+{
+    public partial class Form1 : Form
+    {
+        Microsoft.Office.Interop.Word.Document document = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.axFramerControl1.Open(Path.Combine(System.Windows.Forms.Application.StartupPath, "AppData", "sample.docx"));
+            document = this.axFramerControl1.ActiveDocument as Microsoft.Office.Interop.Word.Document;
+
+            AddSimpleTable(document.Application, document, 10, 3, WdLineStyle.wdLineStyleDashDot, WdLineStyle.wdLineStyleDashDot);
+
+            //在书签处插入文字
+            object oStart = "custom";
+            Range range = document.Bookmarks.get_Item(ref oStart).Range;
+            range.Text = "这里是您要输入的内容";
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            this.axFramerControl1.Close();
+
+            base.OnFormClosing(e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Range range = document.Application.Selection.Range;
+            range.Text = "${userName}";
+            document.Bookmarks.Add("userName", range);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            object oStart = "userName";
+            Range range = document.Bookmarks.get_Item(ref oStart).Range;
+            range.Text = "这里是您要输入的内容";
+        }
+
+        public static void AddSimpleTable(Microsoft.Office.Interop.Word.Application WordApp, Document WordDoc, int numrows, int numcolumns, WdLineStyle outStyle, WdLineStyle intStyle)
+        {
+            Object Nothing = System.Reflection.Missing.Value;
+            //文档中创建表格   
+            Microsoft.Office.Interop.Word.Table newTable = WordDoc.Tables.Add(WordApp.Selection.Range, numrows, numcolumns, ref Nothing, ref Nothing);
+            //设置表格样式   
+            newTable.Borders.OutsideLineStyle = outStyle;
+            newTable.Borders.InsideLineStyle = intStyle;
+            newTable.Columns[1].Width = 100f;
+            newTable.Columns[2].Width = 220f;
+            newTable.Columns[3].Width = 105f;
+
+            //填充表格内容   
+            newTable.Cell(1, 1).Range.Text = "产品详细信息表";
+            newTable.Cell(1, 1).Range.Bold = 2;//设置单元格中字体为粗体   
+                                               //合并单元格   
+            newTable.Cell(1, 1).Merge(newTable.Cell(1, 3));
+            WordApp.Selection.Cells.VerticalAlignment = WdCellVerticalAlignment.wdCellAlignVerticalCenter;//垂直居中   
+            WordApp.Selection.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;//水平居中   
+
+            //填充表格内容   
+            newTable.Cell(2, 1).Range.Text = "产品基本信息";
+            newTable.Cell(2, 1).Range.Font.Color = WdColor.wdColorDarkBlue;//设置单元格内字体颜色   
+                                                                           //合并单元格   
+            newTable.Cell(2, 1).Merge(newTable.Cell(2, 3));
+            WordApp.Selection.Cells.VerticalAlignment = WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+
+            //填充表格内容   
+            newTable.Cell(3, 1).Range.Text = "品牌名称：";
+            newTable.Cell(3, 2).Range.Text = "品牌名称：";
+            //纵向合并单元格   
+            newTable.Cell(3, 3).Select();//选中一行   
+            object moveUnit = WdUnits.wdLine;
+            object moveCount = 5;
+            object moveExtend = WdMovementType.wdExtend;
+            WordApp.Selection.MoveDown(ref moveUnit, ref moveCount, ref moveExtend);
+            WordApp.Selection.Cells.Merge();
+
+
+            newTable.Cell(12, 1).Range.Text = "产品特殊属性";
+            newTable.Cell(12, 1).Merge(newTable.Cell(12, 3));
+
+            //在表格中增加行   
+            WordDoc.Content.Tables[1].Rows.Add(ref Nothing);
+        }
+    }
+}
